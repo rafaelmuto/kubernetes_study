f9d22f04aae1def5eaf709d4766086914e5ac648
rafaelmuto <r.nagahama@gmail.com>
2023-04-08 13:52:48 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "GOSERVER: Hello, I'm %s. I'm %s.", name, age)
+}
