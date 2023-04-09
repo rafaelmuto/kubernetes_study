921ea267c4b6417c6df1d8e79ae6051860f1658d
rafaelmuto <r.nagahama@gmail.com>
2023-04-09 13:23:55 -0300

    configmap volumes

--- server.go
+++ server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -15,4 +18,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "GOSERVER: Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("files/notes.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "my notes file: %s.", string(data))
 }
